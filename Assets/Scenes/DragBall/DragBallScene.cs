3375779b95318f3e94a6a9befd440379aa97a855
slimek <slime573@gmail.com>
2016-04-19 23:22:09 +0800

    Initial revision.

---
+++ Assets/Scenes/DragBall/DragBallScene.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragBallScene : MonoBehaviour
+{
+    private UIRoot m_root;
+
+
+    void Start ()
+    {
+        m_root = GetComponent< UIRoot >();
+
+        var child = gameObject.AddChild();
+        var texture = child.AddComponent< UITexture >();
+        texture.mainTexture = Resources.Load< Texture2D >( "SolidYellow" );
+
+        texture.leftAnchor.target = m_root.transform;
+        texture.leftAnchor.relative = 0;
+
+        texture.rightAnchor.target = m_root.transform;
+        texture.rightAnchor.relative = 1;
+
+        texture.bottomAnchor.target = m_root.transform;
+        texture.bottomAnchor.relative = 0;
+
+        texture.topAnchor.target = m_root.transform;
+        texture.topAnchor.relative = 1;
+    }
+    
+    void Update ()
+    {
+    
+    }
+}
