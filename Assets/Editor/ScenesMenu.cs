bd83919dfdf3a0e8672fc6893a0fe13df3359e09
slimek <slime573@gmail.com>
2017-01-16 22:15:25 +0800

    Implement ScenesMenu

--- Assets/Editor/ScenesMenu.cs
+++ Assets/Editor/ScenesMenu.cs
@@ -6,8 +6,16 @@ using UnityEditor.SceneManagement;
 //
 public static class ScenesMenu
 {
+    [MenuItem("Scenes/Start Scene", false, 0)]
     public static void Start()
     {
+        OpenScene("Start");
+    }
+
+    [MenuItem("Scenes/Title View", false, 1)]
+    public static void Title()
+    {
+        OpenView("Title");
     }
 
     private static void OpenScene(string sceneName)
@@ -26,6 +34,27 @@ public static class ScenesMenu
 
         if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
         {
+            EditorSceneManager.OpenScene(scenePath);
+        }
+    }
+
+    private static void OpenView(string viewName)
+    {
+        var viewPath = string.Format("Assets/Views/{0}.unity", viewName);
+
+        if (EditorSceneManager.GetActiveScene().path == viewPath)
+        {
+            EditorUtility.DisplayDialog(
+                "Hint",
+                string.Format("{0} view is already open", viewName),
+                "Ok"
+            );
+            return;
+        }
+
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene(viewPath);
         }
     }
 }
